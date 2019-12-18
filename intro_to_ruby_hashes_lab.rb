0771d92d7303c59524a7d162668d31566f8f97f6
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-18 16:16:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,14 +13,9 @@ def monopoly
 	monopoly = {
 	  :railroads => {
 	    :pieces => 4
-	    :names => {
-	      
-	    }
-	    :rent_in_dollars => {
-	      
-	    }
+	    :names => {}
 	  }
-	}}
+	}
 end
 
 
